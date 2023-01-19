62513c82d512545548b54aafbe828e91adf1659a
tommy3713 <tommy3713@gmail.com>
2023-01-19 11:27:21 -0800

    feat: add filter feature

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { ApiService } from './api.service';
 import { Todo } from './todos/todos'
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './modal/modal.component';
+import { lastValueFrom, pipe } from 'rxjs';
+
+enum Status{
+  Todo = 'Todo',
+  InProgress = 'In Progress',
+  Complete = 'Complete'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,17 +20,22 @@ import { ModalComponent } from './modal/modal.component';
 export class AppComponent {
   title = 'TodoList';
   taskName = '';
-  status = 'Todo';
-  itemCanEdit = [false, false, false];
-  todos: Todo[] = [
-
-  ];
+  checkBoxStatus = {
+    todo: true,
+    inProgress: true,
+    complete: true
+  }
+  
+  // {id:123, taskName:"dohw", status:"in progress"}
+  todos: Todo[] = [];
   constructor(
     private apiService: ApiService,
     private modalService: NgbModal
-  ){}
+  ){
+
+  }
   generateID = () => new Date().getTime().toString();
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Todo[]>) {
     moveItemInArray(this.todos, event.previousIndex, event.currentIndex);
   }
   handleClickAddTask(){
@@ -33,6 +45,10 @@ export class AppComponent {
       status: "Todo"
     }
     this.addTodo(todo);
+    this.taskName = '';
+  }
+  async refreshTodos(){
+    this.todos = await lastValueFrom(this.apiService.getTodos())
   }
   addTodo(todo: Todo){
     this.apiService
@@ -53,15 +69,27 @@ export class AppComponent {
         this.todos.splice(index,1);
       });
   }
+
   openModal(index: number){
     const modalRef = this.modalService.open(ModalComponent);
     modalRef.componentInstance.taskName = this.todos[index].taskName;
-    modalRef.result.then((taskName: string) => {
-      this.todos[index].taskName = taskName;
+    modalRef.result.then((newTaskName: string) => {
+      this.todos[index].taskName = newTaskName;
       this.updateTodo(index);
     });
   }
-  ngOnInit(): void {
+  async handleFilter(){
+    await this.refreshTodos();
+    const filterdTodos: Todo[] = this.todos.filter((todo)=>{
+      if (this.checkBoxStatus.todo && todo.status==='Todo') return true
+      if (this.checkBoxStatus.inProgress && todo.status==='In Progress') return true
+      if (this.checkBoxStatus.complete && todo.status==='Complete') return true
+      return false
+    })
+    this.todos.splice(0, this.todos.length, ...filterdTodos);    
+  }
+  async ngOnInit(): Promise<void> {
+    await this.refreshTodos();
   }
 
 }
