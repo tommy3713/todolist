17a678fcb7e13fc7595b21c4923b35c740f1cbd1
tommy3713 <tommy3713@gmail.com>
2023-01-23 17:26:50 -0800

    feat: add frontend automation test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        HttpClientModule,
+        FormsModule
       ],
     }).compileComponents();
   });
@@ -16,16 +24,19 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'TodoList'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('TodoList');
   });
-
-  it('should render title', () => {
+  it(`should set input type text value'`, fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const taskName = fixture.debugElement.query(By.css("#taskName"));
+    taskName.nativeElement.value = "Do homework";
+    taskName.nativeElement.dispatchEvent(new Event('input'));
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
-  });
+    tick();
+    expect(app.taskName).toBe('Do homework');
+  }));
 });
